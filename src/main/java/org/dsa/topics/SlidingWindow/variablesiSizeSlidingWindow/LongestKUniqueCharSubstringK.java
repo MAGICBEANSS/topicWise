14bb8841941006db6f26ffb8cccaf9de9d16ed73
MAGICBEANSS <piyushpandey2011ece@gmail.com>
2024-03-18 17:52:43 +0530

    Sliding-Window

---
+++ src/main/java/org/dsa/topics/SlidingWindow/variablesiSizeSlidingWindow/LongestKUniqueCharSubstringK.java
@@ -0,0 +1,70 @@
+package org.dsa.topics.SlidingWindow.variablesiSizeSlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestKUniqueCharSubstringK {
+//https://www.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+    public static void main(String[] args) {
+        String S = "aabacbebebe";
+        int k = 3;
+        int z = longestkSubstr(S,k);
+        System.out.println(z);
+
+    }
+
+    public static int longestkSubstr(String s, int k) {
+        // code here
+        int i=0,j=0;
+        int n = s.length();
+        int maxLength = -1;
+        int originalKeys = k;
+
+
+        Map<Character,Integer> lastIndexMap = new HashMap<>();
+        while(j<n) {
+            char ch = s.charAt(j);
+            if(lastIndexMap.containsKey(ch)) {
+                lastIndexMap.put(ch,j);
+                if(k==0) {
+                      maxLength = Math.max(maxLength,j-i+1);
+                }
+            }
+            else {
+                int currentKeysCount = lastIndexMap.keySet().size();
+                if((originalKeys-currentKeysCount)==1) {
+                    lastIndexMap.put(ch,j);
+                    k--;
+                    if(k==0) {
+                        maxLength = Math.max(maxLength, j - i + 1);
+                    }
+                }
+                if(currentKeysCount<originalKeys) {
+                    lastIndexMap.put(ch,j);
+                    k--;
+                    if(k==0) {
+                         maxLength = Math.max(maxLength,j-i+1);
+                    }
+                }
+                else {
+                    boolean removedExistingCharacter  = false;
+                    while (!removedExistingCharacter  && i<j) {
+                        char charAtI = s.charAt(i);
+                        int lastIndexOfElement = lastIndexMap.get(charAtI);
+                        if(lastIndexOfElement==i) {
+                            removedExistingCharacter = true;
+                            lastIndexMap.remove(charAtI);
+                            k++;
+                        }
+                        i++;
+                    }
+                    lastIndexMap.put(ch,j);
+                    k--;
+                    maxLength = Math.max(maxLength,j-i+1);
+                }
+            }
+        j++;
+        }
+        return maxLength;
+    }
+}
--- src/main/java/org/dsa/topics/SlidingWindow/variablesiSizeSlidingWindow/LongestSubstringWithoutRepeatingCharacter.java
+++ src/main/java/org/dsa/topics/SlidingWindow/variablesiSizeSlidingWindow/LongestSubstringWithoutRepeatingCharacter.java
@@ -1,38 +1,58 @@
 package org.dsa.topics.SlidingWindow.variablesiSizeSlidingWindow;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacter {
    // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s = "";
         int z = lengthOfLongestSubstring(s);
         System.out.println(z);
     }
 
     public static int lengthOfLongestSubstring(String s) {
         int n = s.length();
-        Set<Character> IHaveIt = new HashSet<>();
+        Map<Character,Integer> IHaveIt = new HashMap<>();
         int j =0;
         int currlength =0;
-        int maxcount = 0;
-        int i =-1;
+        int maxLength = 0;
+        int i =0;
         while (j<n) {
             Character ch = s.charAt(j);
-            if(IHaveIt.contains(ch)) {
-                i++;
-                int outGoingIndex = i;
+            if(IHaveIt.containsKey(ch)) {
+                /**
+                 * duplicate found
+                 * so we move i to from initial position to the index where it had the same ch
+                 * This index we will found from map. say it is tofindIndex value
+                 *
+                 * now we will iterate from current position of i to tofindIndex
+                 * in this process we will keep on decrementing the count of every char we found while traversing
+                 * we keep on removing from current map too
+                 *
+                 */
+                int duplicateFoundIndex = IHaveIt.get(ch);
+                while (i<=duplicateFoundIndex) {
+                    char chatAtI = s.charAt(i);
+                    decrementOrRemove(IHaveIt,chatAtI);
+                    i++;
+                }
+            }
 
-            }
-            else {
-                j++;
-            }
-            IHaveIt.add(ch);
+            IHaveIt.put(ch,j);
             currlength = j-i+1;
-            maxcount = Math.max(currlength,maxcount);
+            maxLength = Math.max(maxLength,currlength);
             j++;
         }
-        return maxcount;
+
+        return maxLength;
+    }
+
+    static void decrementOrRemove(Map<Character, Integer> hm, char ch) {
+        int count = hm.getOrDefault(ch,1);
+        if(count==1) hm.remove(ch);
+        else hm.put(ch,count-1);
     }
 }
